600d9e2bfe45ab13cf513246770b09672c9fb973
Mauricio Lechuga <mlechugasanchez2857@conestogac.on.ca>
2025-03-10 15:31:44 -0400

    Add SplashActivity with splash screen and update manifest

---
+++ app/src/main/java/com/example/mobile2_a2/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.example.mobile2_a2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed((new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }), 3000);
+    }
+}
